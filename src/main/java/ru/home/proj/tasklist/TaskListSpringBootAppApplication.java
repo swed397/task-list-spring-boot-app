a9060274b2a077fba1aec51e86986c94939ba8ce
Rodion <swed197@mail.ru>
2023-05-30 00:33:41 +0500

    init commit

---
+++ src/main/java/ru/home/proj/tasklist/TaskListSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package ru.home.proj.tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskListSpringBootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/home/proj/tasklist/TaskListSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.proj.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
