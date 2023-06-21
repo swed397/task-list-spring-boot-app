0d9fe39e4ffff472edd52f61e297fe484f97d235
Rodion <swed197@mail.ru>
2023-06-22 01:36:34 +0500

    added controller advice handling

--- src/main/java/ru/home/proj/tasklist/repos/UserRepository.java
+++ src/main/java/ru/home/proj/tasklist/repos/UserRepository.java
@@ -1,7 +1,6 @@
 package ru.home.proj.tasklist.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.home.proj.tasklist.entities.User;
