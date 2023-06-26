e2f71c2f4f6e1fc67cc4960ffd18bff9a6a7ba74
Rodion <swed197@mail.ru>
2023-06-27 01:10:02 +0500

    added 1st part of security expression

--- src/main/java/ru/home/proj/tasklist/config/AppConf.java
+++ src/main/java/ru/home/proj/tasklist/config/AppConf.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
