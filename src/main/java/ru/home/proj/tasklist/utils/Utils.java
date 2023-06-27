0915c9e80f6b1b5bdbfbe167d40a2f32f487adcd
Rodion <swed197@mail.ru>
2023-06-28 01:08:50 +0500

    added security expression

--- src/main/java/ru/home/proj/tasklist/utils/Utils.java
+++ src/main/java/ru/home/proj/tasklist/utils/Utils.java
@@ -1,7 +1,6 @@
 package ru.home.proj.tasklist.utils;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import ru.home.proj.tasklist.config.security.JwtEntity;
 
 public class Utils {
